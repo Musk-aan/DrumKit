8043f3556f04d91d74274abeb8246a7d11ffe4c2
Muskan Agarwal <100601050+Musk-aan@users.noreply.github.com>
2023-11-03 23:18:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var n= document.querySelectorAll(".drum").length;
+
+for(var i=0;i<n;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    
+    });
+}
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//audio.play();
+// console.log(this.innerHTML);
+// this.style.color="pink";
+
+// var houseKeeper1={
+//     name: "Pushpa",
+//     age: "25",
+//     experience: "4 years",
+//     work_provided: "washing dishes and mopping of floors",
+// };
+// console.log(houseKeeper1.name);
+
+
+// function HouseKeeper(name, age, experience){
+//     this.name=name;
+//     this.age=age;
+//     this.experience=experience;
+// }
+
+// var housekeeper2=new HouseKeeper("Vivek", 23, " 4years");
+// console.log(housekeeper2);
